981fe9e72036955884f5a693a68f270d6464a4da
ninhdng37 <72867069+ninhdng37@users.noreply.github.com>
2023-04-02 23:14:32 +0700

    Add files via upload

---
+++ repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.xemphim.WebXemPhim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xemphim.WebXemPhim.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
---
+++ repository/DiscountRepository.java
@@ -0,0 +1,9 @@
+package com.xemphim.WebXemPhim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xemphim.WebXemPhim.entity.Discount;
+
+public interface DiscountRepository extends JpaRepository<Discount, Integer>{
+
+}
