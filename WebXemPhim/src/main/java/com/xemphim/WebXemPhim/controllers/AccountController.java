54f04c71421fba658f9c19913bcd875ff66a1ebc
ninhdng37 <72867069+ninhdng37@users.noreply.github.com>
2023-04-12 19:59:57 +0700

    Add files via upload

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/controllers/AccountController.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/controllers/AccountController.java
@@ -1,27 +1,5 @@
 package com.xemphim.WebXemPhim.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.xemphim.WebXemPhim.dto.AccountDTO;
-import com.xemphim.WebXemPhim.service.AccountService;
-
-import jakarta.validation.Valid;
-
-@RestController
-@CrossOrigin
-@RequestMapping("/account")
 public class AccountController {
 	
-	@Autowired
-	private AccountService accountService;
-	
-	@PostMapping("/login")
-	public boolean Login(@RequestBody @Valid AccountDTO accountDTO) {
-		return accountService.findById(accountDTO);
-	}
 }
