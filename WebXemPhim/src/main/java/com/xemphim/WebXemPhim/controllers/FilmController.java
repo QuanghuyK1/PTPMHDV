067eb7142de6e004bc8110a04eb14cc7e0664864
ninhdng37 <72867069+ninhdng37@users.noreply.github.com>
2023-04-19 16:16:32 +0700

    Add files via upload

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/controllers/FilmController.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/controllers/FilmController.java
@@ -3,25 +3,35 @@ package com.xemphim.WebXemPhim.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xemphim.WebXemPhim.dto.FilmDTO;
+import com.xemphim.WebXemPhim.controllers.output.Detail1;
+import com.xemphim.WebXemPhim.converter.FilmConverter;
+import com.xemphim.WebXemPhim.service.CommentService;
 import com.xemphim.WebXemPhim.service.FilmService;
 
 @CrossOrigin
 @RestController
-@RequestMapping(path = "/film")
+@RequestMapping(path = "/home")
 public class FilmController {
 
 	@Autowired
 	FilmService filmService;
 	
-	@GetMapping
-	public FilmDTO film(@RequestBody String filmName) {
-		FilmDTO filmDTO = new FilmDTO();
-		filmDTO = filmService.findByFilmName(filmName);
-		return filmDTO;
+	@Autowired
+	CommentService commentService;
+	
+	@Autowired
+	FilmConverter filmConverter;
+	
+	@GetMapping(value = "film/{filmName}")
+	public Detail1 film(@PathVariable String filmName) {
+		Detail1 detail1 = new Detail1();
+		detail1.setFilmDTO(filmConverter.toDTO(filmService.findOneByFilmName(filmName)));
+		detail1.setFilmDTOs(filmService.findFilmDTOByFilmName(filmName));
+		detail1.setComments(commentService.findCommentDTOByFilmName(filmName));
+		return detail1;
 	}
 }
