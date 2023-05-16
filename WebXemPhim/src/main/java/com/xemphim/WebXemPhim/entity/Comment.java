986f2b25933c9e97e062156d26d19df49d9f7bd8
N19DCCN058 <96779169+N19DCCN058@users.noreply.github.com>
2023-05-16 15:02:53 +0700

    Update Comment.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/entity/Comment.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/entity/Comment.java
@@ -1,31 +1,65 @@
 package com.xemphim.WebXemPhim.entity;
 
 import java.util.Date;
+import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
-	
-	@EmbeddedId
-	private CommentId id;
-	
+
+	@Id
+	@Column(name = "comment_id")
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "film_id")
+	private Film film;
+	@ManyToOne
+	@JoinColumn(name = "account_name")
+	private Account account;
+
 	@Column(name = "comment_content")
-	private String commentContent;	
-	
+	private String commentContent;
+
 	@Column(name = "comment_date")
 	private Date commentDate;
-	
-	public CommentId getId() {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_comment_id")
+	private Comment parentComment;
+
+	public Film getFilm() {
+		return film;
+	}
+
+	public void setFilm(Film film) {
+		this.film = film;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public int getId() {
 		return id;
 	}
 
-	public void setId(CommentId id) {
+	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Comment getParentComment() {
+		return parentComment;
+	}
+
+	public void setParentComment(Comment parentComment) {
+		this.parentComment = parentComment;
 	}
 
 	public String getCommentContent() {
