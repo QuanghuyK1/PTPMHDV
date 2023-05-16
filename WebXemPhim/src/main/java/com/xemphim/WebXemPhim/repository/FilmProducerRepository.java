3fc20976ec094a13b1dacf3b1399b9ae885cdb9b
N19DCCN058 <96779169+N19DCCN058@users.noreply.github.com>
2023-05-16 16:29:49 +0700

    Update FilmProducerRepository.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/FilmProducerRepository.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/FilmProducerRepository.java
@@ -4,5 +4,6 @@ import com.xemphim.WebXemPhim.entity.FilmProducer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FilmProducerRepository extends JpaRepository<FilmProducer, Integer>{
+  FilmProducer findOneByFilmProducerId(int producerId);
 
 }
