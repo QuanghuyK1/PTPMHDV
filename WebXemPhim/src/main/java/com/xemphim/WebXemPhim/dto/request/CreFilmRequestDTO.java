3fb715e411deff63ab58e19ed30b9ff75089e9af
N19DCCN058 <96779169+N19DCCN058@users.noreply.github.com>
2023-05-16 15:10:11 +0700

    Update CreFilmRequestDTO.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/dto/request/CreFilmRequestDTO.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/dto/request/CreFilmRequestDTO.java
@@ -6,9 +6,73 @@ import java.util.List;
 
 public class CreFilmRequestDTO {
     private String name;
+    private String description;
+    private int duration;
+    private boolean odd;
+    private int producer_id;
+    private String release_time;
+    private int nation_id;
+    private int director_id;
+
     private MultipartFile poster;
     private MultipartFile trailer;
     private List<CreEpisodeRequestDTO> episodeRequests;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getRelease_time() {
+        return release_time;
+    }
+
+    public void setRelease_time(String release_time) {
+        this.release_time = release_time;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public boolean isOdd() {
+        return odd;
+    }
+
+    public void setOdd(boolean odd) {
+        this.odd = odd;
+    }
+
+    public int getProducer_id() {
+        return producer_id;
+    }
+
+    public void setProducer_id(int producer_id) {
+        this.producer_id = producer_id;
+    }
+
+    public int getNation_id() {
+        return nation_id;
+    }
+
+    public void setNation_id(int nation_id) {
+        this.nation_id = nation_id;
+    }
+
+    public int getDirector_id() {
+        return director_id;
+    }
+
+    public void setDirector_id(int director_id) {
+        this.director_id = director_id;
+    }
 
     public MultipartFile getPoster() {
         return poster;
