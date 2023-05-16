80ce06e481d4fa5cf9e61bd43868976062986201
N19DCCN058 <96779169+N19DCCN058@users.noreply.github.com>
2023-05-16 16:28:27 +0700

    Update NationRepository.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/NationRepository.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/NationRepository.java
@@ -4,5 +4,6 @@ import com.xemphim.WebXemPhim.entity.Nation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NationRepository extends JpaRepository<Nation, Integer>{
+  Nation findOneByNationId(int nationId);
 
 }
