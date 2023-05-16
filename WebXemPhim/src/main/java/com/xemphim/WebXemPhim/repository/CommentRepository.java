04c4137496efd22380829b785fa6560aee4ad536
N19DCCN058 <96779169+N19DCCN058@users.noreply.github.com>
2023-05-16 15:08:23 +0700

    Update CommentRepository.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/CommentRepository.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/CommentRepository.java
@@ -1,8 +1,20 @@
 package com.xemphim.WebXemPhim.repository;
 
 import com.xemphim.WebXemPhim.entity.Comment;
+import com.xemphim.WebXemPhim.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+
+    List<Comment> findAllByFilm(Film film);
+
+
+    @Query(value = "call web_phim.get_comments_for_film(:id);", nativeQuery = true)
+    List<Object> findCommentsTree(@Param("id") String id);
 
 }
