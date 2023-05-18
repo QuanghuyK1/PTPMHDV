e8550f31c4f0402f3daf01f29d4c0f16e83ef8ed
ninhdng37 <72867069+ninhdng37@users.noreply.github.com>
2023-05-19 06:36:43 +0700

    Update PurchasedFilmPackageRepository.java

--- WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/PurchasedFilmPackageRepository.java
+++ WebXemPhim/src/main/java/com/xemphim/WebXemPhim/repository/PurchasedFilmPackageRepository.java
@@ -1,9 +1,20 @@
 package com.xemphim.WebXemPhim.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+
 import com.xemphim.WebXemPhim.entity.PurchasedFilmPackage;
 import com.xemphim.WebXemPhim.entity.PurchasedFilmPackageId;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+@EnableJpaRepositories
 public interface PurchasedFilmPackageRepository extends JpaRepository<PurchasedFilmPackage, PurchasedFilmPackageId>{
-
+	@Query(value = "CALL get_film_package_for_client(:acc_name);", nativeQuery = true)
+    List<Object[]> getFilmPackageForClient(@Param("acc_name") String acc_name);
+    
+    @Query(value = "CALL get_purchase_history(:acc_name);", nativeQuery = true)
+    List<Object[]> getPurchaseHistory(@Param("acc_name") String acc_name);
 }
